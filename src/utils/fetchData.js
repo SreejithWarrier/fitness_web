4002916b5e3e32b06d6be0026f29a9aa785c75f3
SreejithWarrier <102817090+SreejithWarrier@users.noreply.github.com>
2022-10-01 19:58:29 +0530

    Update fetchData.js
    API

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions ={
   method: 'GET',
   
   headers: {
-    'X-RapidAPI-Key': '52e57a6312msh1f63bc47345f189p16fa69jsna4a74dc74829',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
@@ -10,7 +10,7 @@ export const exerciseOptions ={
  export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '52e57a6312msh1f63bc47345f189p16fa69jsna4a74dc74829',
+    'X-RapidAPI-Key':process.env.REACT_APP_RAPID_API_KEY ,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
